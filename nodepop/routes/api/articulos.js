85a6d0837cfa88040204771274377bb710119843
Sonia Vidal <soniaavidaal@gmail.com>
2023-01-06 18:52:10 +0100

    Se puede buscar y actualizar un anuncio

--- nodepop/routes/api/articulos.js
+++ nodepop/routes/api/articulos.js
@@ -15,6 +15,34 @@ router.get('/', async(req, res, next) => {
     }
 });
 
+//Get ./api/agentes/(id)
+router.get('/:id', async(req, res, next) => {
+    try {
+        const id = req.params.id;
+        const articulo = await Anuncio.findById(id);
+
+        res.json({result: articulo});
+    } catch (error) {
+        next(err);
+    }
+})
+
+//Actualizar anuncio. PUT /api/agentes/(id)(body)
+router.put('/:id', async(req, res, next) => {
+    try {
+        const id = req.params.id;
+        const adData = req.body;
+        const adUpdate = await Anuncio.findOneAndUpdate({_id: id}, adData, {
+            new: true
+        });
+
+        res.json({ result: adUpdate });
+
+    } catch (err) {
+        next(err);
+    }
+})
+
 
 
 
