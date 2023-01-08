0d0c23ce502ef0aa4983c42858354df6cb887b35
Sonia Vidal <soniaavidaal@gmail.com>
2023-01-08 22:05:48 +0100

    Actualizacion contraseña y usuario

--- nodepop/lib/authMiddleware.js
+++ nodepop/lib/authMiddleware.js
@@ -2,7 +2,7 @@ const basicAuth = require('basic-auth');
 
 module.exports = (req, res, next) => {
     const adminUser = basicAuth(req);
-    if(!adminUser || adminUser.name !== 'SoniaBootcamp' || adminUser.pass !== 'keepcoding2223'){
+    if(!adminUser || adminUser.name !== 'Keepcoding' || adminUser.pass !== 'Bootcamp'){
         res.set('WWW-Authenticate', 'Basic realm=Authorization required');
         res.sendStatus(401);
         return;
